13c69cfc47ca5e90fb6129033cd51c49fa41732f
Stan James <stan@wanderingstan.com>
2016-04-23 10:56:32 -0700

    Added test for day of week. No expects parameters specifying days of week to post. 0=Monday

--- bin/go_wotd.py
+++ bin/go_wotd.py
@@ -1,10 +1,23 @@
-import os, pprint
+import sys, os, pprint, datetime, argparse
+
 import praw, trello
 # Our stuff
 import read_env, send_email, trello_2_reddit
 
 # Debug output
 pp = pprint.PrettyPrinter(indent=4)
+
+# Arguments
+parser = argparse.ArgumentParser(description='Post word of the day.')
+parser.add_argument('post_days', metavar='N', type=int, nargs='*',
+                   help='Days of the week to post. Monday is 0 and Sunday is 6.')
+args = parser.parse_args()
+
+# Test day of week
+weekday = datetime.datetime.today().weekday()
+if (weekday not in args.post_days):
+	print ("Skipping because today's day of week (%s) is not in list of post days." % weekday)
+	exit()
 
 if 'BOT_NAME' not in os.environ:
 	# Environment variables are not set, try to read from .env file
@@ -34,7 +47,7 @@ print "Reddit URL is " + reddit_post.url
 message = "Posted card to reddit. Title was:\n%s\nAnd body was:\n%s\nReddit URL is:%s\n\nGobble gobble!" % (
 	trello_card['name'],
 	trello_card['desc'],
-	reddit_post.url)  
+	reddit_post.url)
 send_email.send_email(
 	os.environ['EMAIL_TO'].split(','),
 	"WotD Posted",
